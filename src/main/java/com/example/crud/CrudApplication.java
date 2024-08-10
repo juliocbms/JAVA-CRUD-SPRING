fbeb611d76bc3e7dffe0d9ffa8fbe071c37014f4
julinhouu00 <juliocesar13b@gmail.com>
2024-08-10 13:48:29 -0300

    new commit

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -2,6 +2,8 @@ package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class CrudApplication {
--- src/main/java/com/example/crud/ServInitializer.java
+++ src/main/java/com/example/crud/ServInitializer.java
@@ -1,4 +1,17 @@
 package com.example.crud;
 
-public class ServInitializer {
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+
+public class ServInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+return application.sources((CrudApplication.class));
+    }
+
 }
