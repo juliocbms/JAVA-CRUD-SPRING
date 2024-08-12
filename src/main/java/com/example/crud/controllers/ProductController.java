87ad26161389286ba00c943c90196f89fc194545
julinhouu00 <juliocesar13b@gmail.com>
2024-08-12 12:32:28 -0300

    new commit

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,16 +1,43 @@
 package com.example.crud.controllers;
 
 
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import com.example.crud.domain.product.product;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
+
+
     @GetMapping
     public ResponseEntity getAllProducts(){
-        return ResponseEntity.ok("deu ok");
+
+        var allProducts = repository.findAll();
+
+        return ResponseEntity.ok(allProducts);
     }
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        product newProduct = new product(data);
+        repository.save(newProduct);
+        return  ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateProduct(@PathVariable String id, @RequestBody @Valid RequestProduct data){
+        product newProduct = new product(data);
+        product product = repository.getReferenceById(data.id());
+        product.setName(data.name());
+        product.setPrice_inc_cents(data.price_inc_cents());
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/example/crud/domain/product/product.java
+++ src/main/java/com/example/crud/domain/product/product.java
@@ -1,12 +1,28 @@
 package com.example.crud.domain.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Table(name="product")
 @Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 
 public class product {
+@Id
+@GeneratedValue(strategy =  GenerationType.UUID)
+    private String id;
+
+private  String name;
+
+private Integer price_inc_cents;
+
+public product(RequestProduct requestProduct){
+    this.name = requestProduct.name();
+    this.price_inc_cents = requestProduct.price_inc_cents();
+}
+
 }
